f6c6dc96a7abfee2819111c06c5a1f3e3c5950c2
KamariRoss <kamarimobile@gmail.com>
2020-05-27 16:51:22 -0400

    Refreactor code for one line

--- TwoFer.cs
+++ TwoFer.cs
@@ -4,12 +4,13 @@ public static class TwoFer
 {
     // In order to get the tests running, first you need to make sure the Speak method 
     // can be called both without any arguments and also by passing one string argument.
-    public static string Speak(string name = null)
+    public static string Speak(string name = "you")
     {
-        if (name == null)
-        { return "One for you, one for me."; }
-        else
-        { return $"One for {name}, one for me."; }
+        return $"One for {name}, one for me.";
+        // if (name == null)
+        // { return "One for you, one for me."; }
+        // else
+        // { return $"One for {name}, one for me."; }
 
     }
 }
